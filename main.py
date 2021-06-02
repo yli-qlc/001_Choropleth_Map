5a2961d7dd86ff8fa27c9e2593afffccfe8616c8
yli-qlc <85208491+yli-qlc@users.noreply.github.com>
2021-06-02 12:47:43 -0400

    Update main.py

--- main.py
+++ main.py
@@ -4,15 +4,15 @@ import pandas as pd
 import geopandas as gpd
 
 
-#pd.set_option('display.max_columns', None)
+pd.set_option('display.max_columns', None)
 
 #Read the shape file
-df1 = gpd.read_file(r"...\chn_admbnda_adm1_ocha_2020.shp")
+df1 = gpd.read_file('chn_admbnda_adm1_ocha_2020.shp')
 print(list(df1))
 print(df1.head)
 
 #Read the file with the target variable
-df2 = pd.read_csv(r"...\Age.csv")
+df2 = pd.read_csv('China_Age.csv')
 
 #Merge the two files
 merged=pd.merge(df1, df2, on='ADM1_PCODE')
@@ -25,4 +25,5 @@ print (merged.dtypes)
 fig, ax = plt.subplots(1, figsize=(10,6))
 merged.plot(column='Dependency_Ratio', cmap='Reds',  linewidth=1, ax=ax, edgecolor='0.9', legend=True)
 ax.axis('off')
+plt.title("Dependency Ratio Of The Older Population")
 plt.show()
